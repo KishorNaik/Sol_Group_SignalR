90ff67b0c33bfc9b7975cf5e4145e166647abbc1
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-20 21:24:51 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Hubs/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Hubs
+{
+    public interface IChatClient
+    {
+        Task SendMessageToGroupJsMethod(string user, string message);
+    }
+
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task JoinGroup(string group)
+        {
+            await base.Groups.AddToGroupAsync(Context.ConnectionId, group);
+        }
+
+        public async Task SendMessage(string group, string user, string message)
+        {
+            //await base.Clients.All.SendAsync("SendMessageToClientJsMethod", user, message);
+            await base.Clients.Groups(group).SendMessageToGroupJsMethod(user, message);
+        }
+    }
+}
